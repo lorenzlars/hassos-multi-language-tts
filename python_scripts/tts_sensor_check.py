ab8123bb203e41070a1ba584b6e13b6abd4c01a9
Lars Lorenz <49837899+lorenzlars@users.noreply.github.com>
2020-07-31 09:04:29 +0200

    Update tts_sensor_check.py

--- python_scripts/tts_sensor_check.py
+++ python_scripts/tts_sensor_check.py
@@ -48,18 +48,6 @@ def build_strings(sensors, translations):
     return full_translations
 
 
-# Replace 'Umlaute' with unicode
-def unicode_replacer(text):
-    text = text.replace('ä', '&#228;')
-    text = text.replace('ö', '&#246;')
-    text = text.replace('ü', '&#252;')
-    text = text.replace('Ä', '&#196;')
-    text = text.replace('Ö', '&#214;')
-    text = text.replace('Ü', '&#220;')
-
-    return text
-
-
 # Build the SSML
 def build_ssml(strings):
     first = True
@@ -73,7 +61,7 @@ def build_ssml(strings):
 
         body = body + \
             xml_voice_format.format(
-                name=voices[key], content=unicode_replacer(value))
+                name=voices[key], content=value)
 
     return xml_body_format.format(content=body)
 
